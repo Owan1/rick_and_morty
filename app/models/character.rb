de24baffd1a37efec7e64a2095ee699672c9275e
Daniel Román <danirocar@hotmail.com>
2020-10-02 12:45:09 +0200

    Add Validations to Character and Episode
    Character:
    Validation of uniqueness name field
    Validate Episode associated
    
    Episode:
    Validation of uniqueness number field

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ApplicationRecord
     has_and_belongs_to_many :episodes
+
+    validates :name, uniqueness: { case_sensitive: false }
+    validates :status, presence: true
+    validates_associated :episodes
 end
