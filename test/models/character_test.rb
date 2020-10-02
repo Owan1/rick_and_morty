c84409186e3f85828b7ae098f074993b55f77c81
Daniel Román <danirocar@hotmail.com>
2020-10-02 12:47:11 +0200

    Test Character Validations

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Should not allow duplicate character name" do
+    Character.create(name: "rickyrillo", status: "Unknown")
+    character = Character.new(name: "rickyrillo", status: "Unknown")
+    assert_not character.save, "Saved a duplicate Character name"
+  end
+
+  test "Should not allow no presence of status field" do
+    character = Character.new(name: "rickyrillo_nostatus")
+    assert_not character.save, "Saved a Character with no status"
+  end
+  
 end
