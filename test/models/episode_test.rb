c224635c87485ff7ba80ff25a5d69db2a6bddf44
Daniel Román <danirocar@hotmail.com>
2020-10-02 12:47:22 +0200

    Test Episode Validation

--- test/models/episode_test.rb
+++ test/models/episode_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class EpisodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Should not allow duplicate episode number" do
+    Episode.create(number: 1)
+    episode = Episode.new(number: 1)
+    assert_not episode.save, "Saved a duplicate Episode number"
+  end
+
 end
