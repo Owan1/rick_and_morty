8b3e753e466b835d1168cd9a9762f8dff3ac5f97
Daniel Román <danirocar@hotmail.com>
2020-10-03 16:44:49 +0200

    Update routes.rb
    - root page redirects to character#index
    - any other routes redirect to character#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :characters
+  #resources :characters
+  root to: 'characters#index'
+  #Make sure there is no RoutesError and it is redirected to Home Page
+  match '*path' => redirect('/'), via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
