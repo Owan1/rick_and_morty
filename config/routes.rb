7363625e935840b2a559c7ac245eef43a2b156a1
Daniel Román <danirocar@hotmail.com>
2020-10-04 14:56:25 +0200

    Added some comments into routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #resources :characters
+  ##Set Character#Index as the Root Page
   root to: 'characters#index'
-  #Make sure there is no RoutesError and it is redirected to Home Page
+  ##Make sure there is no RoutesError and it is redirected to Home Page (Character#Index)
   match '*path' => redirect('/'), via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
